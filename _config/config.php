b2b80f9090e562b58fb0b4284c4cfac1a621de0b
Fatima-ezzahra <fatimaezzahrazahid7@gmail.com>
2020-07-11 00:15:39 +0100

    structureMVC+vendors

---
+++ _config/config.php
@@ -0,0 +1,27 @@
+<?php
+
+
+//website information 
+define("WEBSITE_TITLE","Page builder");
+define("WEBSITE_NAME", "Page builder");
+define("WEBSITE_URL", "https://pagebuilder.com");
+define("WEBSITE_DESCRIPTION", "Blog for users");
+define("WEBSITE_KEYWORDS", "");
+
+
+
+
+//facebook open graph tags
+define("WEBSITE_FACEBOOK_NAME", "");
+define("WEBSITE_FACEBOOK_DESCRIPTION", "");
+define("WEBSITE_FACEBOOK_URL", "");
+define("WEBSITE_FACEBOOK_IMAGE", "");
+
+//db informations
+define("DB_HOST", "localhost");
+define("DB_NAME", "test");
+define("DB_USER", "root");
+define("DB_PASS", "");
+
+
+
---
+++ views/includes/footer.php
@@ -0,0 +1,8 @@
+<footer>
+<script src="assets/vendors/js/jquery.min.js"> </script>
+<script src="assets/vendors/js/poper.js"></script>
+<script src="assets/vendors/js/bootstrap.min.js"></script>
+<script src="assets/vendors/js/script.js"></script>
+<script src="assets/vendors/fontawesome5/js/solid.min.js"></script>
+<script src="assets/vendors/fontawesome5/js/fontawesome.min.js"></script>
+</footer>
